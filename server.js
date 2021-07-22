3bcad427267083837488eec0d2ec1c155cdcf6e0
stormsidali2001 <53428196+stormsidali2001@users.noreply.github.com>
2021-07-22 22:17:43 +0500

    Add files via upload

--- server.js
+++ server.js
@@ -9,11 +9,25 @@ dotenv.config();
 
 //
 let app  =express();
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+app.use(allowCrossDomain);
 app.use(cors())
 let server= http.createServer(app);
 const io = require("socket.io")(server,{
     cors:{
-        origin: "*",    
+        origin: ["*"],    
         methods: ["GET", "POST"]  
     }
 });
