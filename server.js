c5d78c5cda98690027d4fed156f38a29e18b1b1c
stormsidali2001 <53428196+stormsidali2001@users.noreply.github.com>
2021-07-22 20:48:52 +0500

    Add files via upload

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const io = require("socket.io")(server,{
 
 const publicPath    = path.join(__dirname, '/public');
 app.use(express.static(publicPath));
-const port =  5001;
+const port =  3000;
 server.listen(port,()=>{
     console.log(`server is running on ${port}`)
 });
