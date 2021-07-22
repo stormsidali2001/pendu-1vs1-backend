986b4e5d5e2a2808fb436fc2516babb743d24802
stormsidali2001 <53428196+stormsidali2001@users.noreply.github.com>
2021-07-22 19:41:55 +0500

    Add files via upload

---
+++ public/precache-manifest.2f8ab404cc9c58f6a2dd37d7960da703.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e25dd53d648c51b44d43c1a0648b801d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca51b78b058e62a557b2",
+    "url": "/static/css/main.e4a9c062.chunk.css"
+  },
+  {
+    "revision": "4f0782ca2795ed603a46",
+    "url": "/static/js/2.14f018ad.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/static/js/2.14f018ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca51b78b058e62a557b2",
+    "url": "/static/js/main.7ff7f56c.chunk.js"
+  },
+  {
+    "revision": "bb5b94e5bf8a2ac4b1d8",
+    "url": "/static/js/runtime-main.6fd1fea2.js"
+  }
+]);
